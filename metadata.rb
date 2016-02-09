5d0c8f0cfaa57236035944a6a431f0a754856b5e
Andrew Klaus <andrewklaus@gmail.com>
2016-02-08 17:13:52 -0700

    Testing

--- metadata.rb
+++ metadata.rb
@@ -6,12 +6,12 @@ description      'Installs/Configures the Network Mapping Tool'
 long_description IO.read(File.join(File.dirname(__FILE__), 'README.md'))
 version          '0.8.0' # (Feb 2016 sprint)
 
+depends "apt"
 depends "database"
 depends "maven"
 depends "postgresql"
 depends "postgis"
 depends "ssh_known_hosts"
 depends "tomcat"
-depends "apt"
 
 recipe "default",         ""
